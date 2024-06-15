ab463870ba0ed7b7241d95d36a33176187819d7d
mihkelvasar <144525436+mihkelvasar@users.noreply.github.com>
2024-06-14 18:23:13 -0700

    Add output color selecting. Data passes but does nothing. Might require Pillow.

--- qr-app-ref.py
+++ qr-app-ref.py
@@ -8,6 +8,106 @@ import qrcode
 # refactoridea - reverse a qrcode parse qr code into a string and write to cmd or file
 # refactoridea - rewrite all in JS (after figuring out tkinter and turning it into an exe)
 
+
+inputacceptance = '''Input accepted and verified. 
+---------------------------'''
+
+def colorvaluechecker(x):
+    for i in x:
+        if i < 0 or i > 255:
+            print("color value out of bounds!")
+            return 1
+        else:
+            return 0
+
+def colorsetter():
+    chosencolors = None
+    colors = None
+    print("here you can select foreground and background colors different from base.")
+    print('''A: proceed with default color scheme.
+            B: specify foreground and background color.
+            [any other]: reloads this section.  ''')
+    colorchooser = input("A or B? ").lower()
+    match colorchooser:
+        case "a": 
+            colors = chosencolors
+        case "b": 
+            colors = coloroptions()
+        case _: colorsetter()
+    return colors
+
+def foregroundcolor():
+    forecolor = ()
+    forecolor1 = []
+    print("Foreground color values")
+    time.sleep(1)
+    try:
+        foreground = list((input("enter foreground color as RGB values separated by a comma: ")).split(","))
+    except: 
+        print(f'''{foreground} cannot be cast as a list separated by commas.
+              Therefore you made an error in input. 
+              This section will reload.''')
+        foregroundcolor()
+
+    for i in foreground:
+        try:
+            i1 = int(i)
+            forecolor1.append(i1)
+        except:
+            print("elements in entered tuple are not numbers as they cannot be cast as ints. Rerun.")
+            foregroundcolor()
+    
+    foreground = tuple(forecolor1)
+
+    if colorvaluechecker(foreground) == 0:
+        forecolor = foreground
+        print(inputacceptance)
+        return forecolor
+    else:
+        print("This section will run again.")
+        foregroundcolor()       
+
+def backgroundcolor():
+    backcolor = ()
+    backcolor1 = []
+    print("Background color values")
+    time.sleep(1)
+    try:
+        background = list((input("enter background color as RGB values separated by a comma: ")).split(","))
+    except: 
+        print(f'''{background} cannot be cast as a list separated by commas.
+              Therefore you made an error in input.
+              This section will reload.''')
+        backgroundcolor()
+
+    for i in background:
+        try:
+            i1 = int(i)
+            backcolor1.append(i1)
+        except:
+            print("elements in entered tuple cannot be cast as ints. Rerun.")
+            backgroundcolor()
+    
+    background = tuple(backcolor1)
+
+    if colorvaluechecker(background) == 0:
+        backcolor = background
+        print(inputacceptance)
+        return backcolor
+    else:
+        print("This section will run again.")
+        backgroundcolor()
+
+def coloroptions():
+    print("colors are specified by RGB values inserted as a tuple")
+    print("for example: '0, 0, 0' is black while '255, 255, 255' is for white")
+    print("all other color values for 16,78 million shades fall in between these values")
+    time.sleep(2)
+    foreground = foregroundcolor()
+    background = backgroundcolor()
+    colors = foreground, background
+    return colors
+
 def int_checker(x):
     try:
         y = int(x)
@@ -33,8 +133,7 @@ def manual_version():
                 return versiondata
             case _: manual_version()
     else:
-        print("input accepted and verified")
-        print("---------------------------")
+        print(inputacceptance)
         return versiondata
 
 def manual_ec():
@@ -59,8 +158,7 @@ def manual_ec():
                 return ec_level
             case _: manual_ec()
     else:
-        print("input accepted and verified")
-        print("---------------------------")
+        print(inputacceptance)
         return  ec_level
 
 def manual_boxsize():
@@ -81,8 +179,7 @@ def manual_boxsize():
                 return man_box_size
             case _: manual_boxsize()
     else:
-        print("input accepted and verified")
-        print("---------------------------")
+        print(inputacceptance)
         return man_box_size
 
 def manual_border():
@@ -104,8 +201,7 @@ def manual_border():
                 return man_border
             case _: manual_border()
     else:
-        print("input accepted and verified")
-        print("---------------------------")
+        print(inputacceptance)
         return man_border
 
 def manual_settings():
@@ -170,23 +266,26 @@ def text_to_qr():
     linecounter = None
     conversion_params = settings_setter()
     targetdata = input("Enter string to be converted (or leave blank for text file prompt): ")
+    targetfile = None
     if targetdata == "":
         targetfile = input("Enter name of file (with extension) containing target text: ")
     outputname = input("Enter name (w/o extension) of the output file. Can be left blank: ")
+    colors = colorsetter()
     if outputname == "":
         outputname = "text_to_qrcode"
     if targetfile != None:
-        qr_convert(readfrom_file(targetfile), outputname, conversion_params, linecounter)
+        qr_convert(readfrom_file(targetfile), outputname, conversion_params, linecounter, colors)
     else:
-        qr_convert(targetdata, outputname, conversion_params, linecounter)
+        qr_convert(targetdata, outputname, conversion_params, linecounter, colors)
 
 def batch_to_qr():
     conversion_params = settings_setter()
     targetdata = input("Enter name of the source file with extension: ")
     outputname = input("Enter name (w/o extension) for the the output files. Can be left blank: ")
+    colors = colorsetter()
     if outputname == None:
         outputname = "batch_to_qrcode"
-    batch_read(targetdata, outputname, conversion_params)
+    batch_read(targetdata, outputname, conversion_params, colors)
         
 
 def batch_read(filename, *args):
@@ -198,7 +297,8 @@ def batch_read(filename, *args):
             linecounter += 1
             outputname = args[0]
             settings = args[1]
-            qr_convert(targetstring, outputname, settings, linecounter)
+            color = args[2]
+            qr_convert(targetstring, outputname, settings, linecounter, color)
 
 
 def readfrom_file(filename):
@@ -206,9 +306,15 @@ def readfrom_file(filename):
         targetstring = s.read()
     return targetstring
 
-def qr_convert(data, name, settings, linecounter):
-    basic_back = (255, 195, 235)
-    basic_fill = (55, 95, 35)
+def qr_convert(data, name, settings, linecounter, colors):
+    
+    if colors != None:
+        basic_back = colors[1]
+        basic_fill = colors[0]
+    else:
+        basic_back = (255, 195, 235)
+        basic_fill = (55, 95, 35)
+
 
     fitmentdict0 = {"fit": False}
     fitmentdict1 = {"fit": True}
@@ -230,13 +336,14 @@ def qr_convert(data, name, settings, linecounter):
             case "y": qr_target.make(**fitmentdict1)
             case _: print("This program will now exit")
     
-
     img = qr_target.make_image(back_color=basic_back, fill_color=basic_fill)
     
     if linecounter == None:
         img.save(f"{name}.png")
+        print(f"qr-code named '{name}.png' saved successfully!")
     else:
         img.save(f"{name}-{linecounter}.png")
+        print(f"qr-code named '{name}-{linecounter}.png' saved successfully!")
 
     # refactoridea - instead of 3 fitmentdicts, just alter one dict
     # refactoridea - pass back and fill color values as dicts
